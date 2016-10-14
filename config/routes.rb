7c662b81da4a53ca0aa70da0e9227d96daca65b2
Next <nextacademy@email.com>
2016-10-14 04:16:25 -0700

    integrated omniath

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  resources :users, only: [:show, :edit, :update, :destroy] 
+
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
